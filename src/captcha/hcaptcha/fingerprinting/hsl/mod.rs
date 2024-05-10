0a2a0153d5fb723224cca59f6741f3b6b16baa1a
dort. <122418336+discord-trollering@users.noreply.github.com>
2024-05-10 02:08:09 -0400

    yes

--- src/captcha/hcaptcha/fingerprinting/hsl/mod.rs
+++ src/captcha/hcaptcha/fingerprinting/hsl/mod.rs
@@ -4,15 +4,19 @@ use super::jwt::parse_jwt;
 use crate::commons::error::DortCapResult;
 use super::PoWChallenge;
 
-pub struct HSL {
-
-}
+pub struct HSL {}
 
 #[async_trait]
 impl PoWChallenge for HSL {
     async fn get_proof(&self, jwt: &str) -> DortCapResult<String> {
         let decoded = parse_jwt(jwt).await?;
-        // Use mint_wasm as it has the 'custom' date format hCaptcha uses.
+        // Use mint_wasm as it has the so-called 'custom' date format hCaptcha uses.
         Ok(Stamp::mint_wasm(Some(&*decoded.data), Some(decoded.stamp_difficulty), None, None, None, false)?.to_string())
+    }
+}
+
+impl Default for HSL {
+    fn default() -> Self {
+        Self {}
     }
 }
