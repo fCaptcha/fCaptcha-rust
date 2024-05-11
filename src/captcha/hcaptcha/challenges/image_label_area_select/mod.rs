1c461a9d78c1e284b2e7d4e0706c565118772a6b
dort. <122418336+discord-trollering@users.noreply.github.com>
2024-05-11 18:22:00 -0400

    last commit probably.

--- src/captcha/hcaptcha/challenges/image_label_area_select/mod.rs
+++ src/captcha/hcaptcha/challenges/image_label_area_select/mod.rs
@@ -1,20 +1,44 @@
 pub mod structs;
-use rocket::async_trait;
-use serde_json::{json, Value};
+
+use std::str::FromStr;
+use redis::{
+    aio::ConnectionManager,
+    AsyncCommands,
+    RedisResult
+};
 use super::{
     super::sessions::structs::Task,
+    constants::ANSWERS,
     Challenge
 };
+use rocket::async_trait;
+use serde_json::{json, Value};
+use crate::captcha::hcaptcha::sessions::HCaptchaSession;
 use self::structs::Answer;
 
-pub struct ImageLabelAreaSelect {
-    pub answers: Vec<Answer<Vec<Value>>>
+pub struct ImageLabelAreaSelect<'a> {
+    pub answers: Vec<Answer<Value>>,
+    session: &'a HCaptchaSession
 }
 
 #[async_trait]
-impl Challenge for ImageLabelAreaSelect {
-    async fn populate_answers_from_database(&mut self, task: Vec<&Task>) {
-        todo!()
+impl Challenge for ImageLabelAreaSelect<'_> {
+    async fn populate_answers_from_database(&mut self, tasks: Vec<&Task>) {
+        let mut database = ConnectionManager::clone(ANSWERS.get().await);
+        for task in tasks {
+            let task_hash = &*task.task_hash;
+            let result: RedisResult<String> = AsyncCommands::get(&mut database, format!("Image Challenges:{0}:{1}", self.session.get_requester_question().await, task_hash)).await;
+            match result {
+                Ok(ref text) => {
+                    for mut answer in &mut self.answers {
+                        if let Ok(value) = Value::from_str(text) {
+                            answer.task_answer = value;
+                        }
+                    }
+                }
+                Err(_) => {}
+            }
+        }
     }
 
     async fn get_answers(&self) -> Value {
--- src/captcha/hcaptcha/sessions/mod.rs
+++ src/captcha/hcaptcha/sessions/mod.rs
@@ -1,16 +1,58 @@
 pub mod structs;
 
+use std::time::Duration;
+use reqwest::{Client, ClientBuilder, Proxy};
 use rocket::serde::{Deserialize, Serialize};
 use serde_json::Value;
+use DortCapError::DetailedInternalErr;
+use crate::commons::error::DortCapError::CodeErr;
+use crate::commons::error::{DortCapError, DortCapResult};
+use crate::PROXIES;
 
 pub struct HCaptchaSession {
-    pub site_key: String,
-    pub site_url: String,
-    pub rq_data: String
+    site_key: String,
+    site_url: String,
+    rq_data: Option<String>,
+    client: Client
+}
+
+macro_rules! conv_option {
+    ($x:expr) => {
+        match ($x) {
+            Some(t) => {
+                Ok(t)
+            },
+            None => {
+                Err(DetailedInternalErr("UNWRAP_FAILED"))
+            }
+        }
+    };
 }
 
 impl HCaptchaSession {
+
+    pub async fn new(site_key: &str, site_url: &str, rq_data: Option<&str>) -> DortCapResult<HCaptchaSession> {
+        let proxies = PROXIES.read().await;
+        let proxy = conv_option!(fastrand::choice(&*proxies))?;
+        let client = ClientBuilder::new()
+            .proxy(Proxy::all(proxy)?)
+            .timeout(Duration::from_secs(30))
+            .cookie_store(true)
+            .build()?;
+        Ok(HCaptchaSession {
+            site_key: String::from(site_key),
+            site_url: String::from(site_url),
+            rq_data: rq_data.map(|rq_data| {
+                String::from(rq_data)
+            }),
+            client,
+        })
+    }
+
+    pub async fn get_requester_question(&self) -> String {
+        unimplemented!()
+    }
     pub async fn get_language(&self) -> String {
-        String::from("")
+        unimplemented!()
     }
 }
